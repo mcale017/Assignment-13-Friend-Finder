32b439da0eafb679dd49a6b86363a67f56d19a66
Ryan <mcale017@umn.edu>
2018-06-11 17:57:27 -0500

    initial set-up

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
